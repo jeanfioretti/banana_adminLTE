55913504d45efa1ad05f00852e8766dd2b72513b
nestor <nmoreno@overcom.net>
2018-07-16 15:38:36 -0400

    commit

--- src/app/banana/components/contact-list/contact-list.component.ts
+++ src/app/banana/components/contact-list/contact-list.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { tokenUtil } from '../../utils/tokenUtil';
+import { notifyManage, showNotification } from '../../utils/notifyUtil';
 import { Contact } from '../../models/contact';
 declare var $: any;
+
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent implements OnInit {
+	loading = false;
+	body : any;
 	collapsed : boolean = false;
 	@Input() contacts : Array<any> = [];
+	@Output() contactArchived = new EventEmitter<any>();
 	contact_list : Contact = new Contact();
 	type_view : number;	
 
-	constructor() { }
+	constructor(public http: HttpClient, public router: Router, private _activeRoute: ActivatedRoute) { }
 
 	ngOnInit() {
   	}
@@ -39,6 +47,65 @@ export class ContactListComponent implements OnInit {
 	cleanContact(clean_contact) {
 		this.type_view = 3;
 		this.contact_list = clean_contact;
+	}
+
+	archivedContact(contact, archived): void {
+		this.loading = true;
+		showNotification("Archivando contacto", 2);
+		const body = {
+			authorization: window.location.origin,
+			user_id: sessionStorage.getItem('user_id'),
+			token: sessionStorage.getItem('user_token'),
+			app:'bananaCli',
+			contact_id : contact.id,
+			archived: archived
+		};
+
+		this.http.post('http://localhost:8000/api/contacts/archived', body).toPromise().then(
+			result => {
+				//this.body = result;
+				showNotification('archivado con exito', 1);
+				contact.archived = archived;
+				this.contactArchived.emit(contact);
+				this.loading = false;
+			},
+			msg => {
+				if (msg.status == 406) {
+					tokenUtil(this.router);
+				}
+				this.loading = false;
+				notifyManage(msg);
+			}
+		);
+	}
+
+	deleteContact (contact) {
+		this.loading = true;
+		showNotification("Eliminando contacto", 2);
+		const body = {
+			authorization: window.location.origin,
+			user_id: sessionStorage.getItem('user_id'),
+			token: sessionStorage.getItem('user_token'),
+			app:'bananaCli',
+			contact_id : contact.id
+		};
+
+		/*this.http.post('http://localhost:8000/api/contacts/archived', body).toPromise().then(
+			result => {
+				//this.body = result;
+				showNotification('archivado con exito', 1);
+				contact.archived = archived;
+				this.contactArchived.emit(contact);
+				this.loading = false;
+			},
+			msg => {
+				if (msg.status == 406) {
+					tokenUtil(this.router);
+				}
+				this.loading = false;
+				notifyManage(msg);
+			}
+		);*/
 	}
 
 }
